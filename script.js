1474e4ab1d94de9309ba51fc29e4cbcd5d5c3ca7
Jarede712 <jaredeic@yahoo.com>
2024-02-02 23:38:12 -0800

    added a basic template and input variables to improve output

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 // dependencies
 const { OpenAI } = require('@langchain/openai');
-require('dotenv').config();
 const inquirer = require('inquirer');
+require('dotenv').config();
+const { PromptTemplate } = require('@langchain/core/prompts');
 
-// creates and stores a wrapper for the OpenAI package along with basic configuration
+// Creates and stores a wrapper for the OpenAI package along with basic configuration
 const model = new OpenAI({
-  openAIApiKey: process.env.OPEN_AI_API_KEY,
+  openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
   model: 'gpt-3.5-turbo',
 });
 
+// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => {
   try {
-    const res = await model.call(
-      input
-      // 'How do you capitalize all characters of a string in JavaScript?' // uncomment to run promptFunc by itself
-    );
+    // Instantiation of a new object called "prompt" using the "PromptTemplate" class
+    const prompt = new PromptTemplate({
+      template:
+        'You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}',
+      inputVariables: ['question'],
+    });
+    const promptInput = await prompt.format({
+      question: input,
+    });
+    const res = await model.call(promptInput);
     console.log(res);
   } catch (err) {
     console.error(err);
   }
 };
 
-// promptFunc(); // uncomment to run the promptFunc by itself (comment out the init function)
-
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
 const init = () => {
   inquirer
     .prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'ask a coding question',
+        message: 'Ask a coding question:',
       },
     ])
     .then((inquirerResponse) => {
@@ -38,4 +45,5 @@ const init = () => {
     });
 };
 
+// Calls the initialization function and starts the script
 init();
