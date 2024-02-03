0c7d231412377b48ac92460e04295199e607a763
Jarede712 <jaredeic@yahoo.com>
2024-02-03 00:51:30 -0800

    added output parser to narrow down the output of the AI to JS code that answers the users question and an explanation of that code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const { OpenAI } = require('@langchain/openai');
 const inquirer = require('inquirer');
 require('dotenv').config();
 const { PromptTemplate } = require('@langchain/core/prompts');
+const { StructuredOutputParser } = require('langchain/output_parsers');
 
 // Creates and stores a wrapper for the OpenAI package along with basic configuration
 const model = new OpenAI({
@@ -11,20 +12,28 @@ const model = new OpenAI({
   model: 'gpt-3.5-turbo',
 });
 
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  code: "Javascript code that answers the user's question",
+  explanation: 'detailed explanation of the example code provided',
+});
+
+const formatInstructions = parser.getFormatInstructions();
+
 // Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => {
   try {
     // Instantiation of a new object called "prompt" using the "PromptTemplate" class
     const prompt = new PromptTemplate({
       template:
-        'You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}',
+        'You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}',
       inputVariables: ['question'],
+      partialVariables: { format_instructions: formatInstructions },
     });
     const promptInput = await prompt.format({
       question: input,
     });
     const res = await model.call(promptInput);
-    console.log(res);
+    console.log(await parser.parse(res));
   } catch (err) {
     console.error(err);
   }
